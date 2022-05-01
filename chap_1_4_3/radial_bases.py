756fa110287dfd3317f61194dc55a5be6b6988cf
luong12320 <nguyenminhluong14092001@gmail.com>
2022-05-02 01:17:54 +0700

    + Add file python script for Radial Base Function

---
+++ chap_1_4_3/radial_bases.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def radial_bases(psi, C, p=2):
+    bases = []
+    for c in C:
+        def func(x,c=c):
+            return psi(np.linalg.norm(x-c,p))
+        bases.append(func)
+    return bases
