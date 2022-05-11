79ad8823635852f7633345733d3d55c6fb95719a
luong12320 <nguyenminhluong14092001@gmail.com>
2022-05-11 12:40:39 +0700

    Little Modify to make uniqueu

--- chap_1_4_3/polynomial_bases.py
+++ chap_1_4_3/polynomial_bases.py
@@ -2,16 +2,29 @@ import numpy as np
 import itertools
 
 def polynomial_bases_1d(i,k):
-    f = lambda x: np.power(x[i],np.arange(0,k+1,1))
+    def func(x):
+        f = []
+        for p in np.arange(0, k+1, 1):
+            f.append(np.power(x[i], p))
+        return np.array(f)
+
+    f = func
     return f
 
 def polynomial_bases(n,k):
-    bases = [ polynomial_bases_1d(i, k) for i in range(n)]
+    bases = []
+    for i in range(n):
+        bases.append(polynomial_bases_1d(i, k))
+    
     terms = []
-    for ks in itertools.product(*[np.arange(0,k+1) for i in range(n)]):
+    for ks in itertools.product(*[np.arange(0, k+1) for i in range(n)]):
         if np.sum(ks) <= k:
             def func(x, ks=ks):
-                return np.prod([b(x)[j] for j, b in zip(ks,bases)])
+                term = 1
+                for j, b in zip(ks, bases):
+                    term *= b(x)[j]
+                return term
+
             terms.append(func)
 
     return terms
