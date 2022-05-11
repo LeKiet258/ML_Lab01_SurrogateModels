da27dfcee75dfa9ba9577b1ed27349139d30e174
luong12320 <nguyenminhluong14092001@gmail.com>
2022-05-11 12:42:06 +0700

    Nothing change

--- chap_1_4_3/radial_bases.py
+++ chap_1_4_3/radial_bases.py
@@ -3,7 +3,7 @@ import numpy as np
 def radial_bases(psi, C, p=2):
     bases = []
     for c in C:
-        def func(x,c=c):
-            return psi(np.linalg.norm(x-c,p))
+        def func(x, c=c):
+            return psi(np.linalg.norm(x-c, p))
         bases.append(func)
     return bases
